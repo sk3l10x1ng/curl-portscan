067e16085016bc690aea5da2e08e133864de5688
Prudhvi Depuru <mr4obot@gmail.com>
2025-03-25 00:32:29 +0530

    added TODO list feature

--- curl-portscan.sh
+++ curl-portscan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# curl-portscan.sh -- by Daniel Roberson @dmfroberson
-#
 # Portscans a host using curl, because its almost always available.
 # It is no replacement for nmap, but gets the job done!
 #
@@ -10,22 +8,28 @@ DEFAULT_PORTS="1-1024"
 DEFAULT_TIMEOUT=1
 PORTINDEX=""
 VERBOSE=0
+DEFAULT_DELAY=2
+LOG_FILE=""
+RANDOMIZE=0
+FAST_SCAN=0
+DNS_DETECT=0
 
-echo "[+] curl-portscan.sh by Daniel Roberson @dmfroberson"
+
 echo
 
 usage() {
     echo "usage: $0 -t <target> -p <ports> [-m <timeout>] [-h]"
-    echo -e "\t-t <target>\t-- singular hostname to scan"
+    echo -e "\t-t <target>\t-- target host/IP/CIDR range to scan"
     echo -e "\t-p <ports>\t-- ports to scan. ex: 1-1024,1055,3333-4444"
     echo -e "\t-m <timeout>\t-- curl timeout in seconds"
     echo -e "\t-v\t\t-- toggle verbose output"
-    echo -e "\t-h\t\t-- this help menu"
-
-    exit 1
+    echo -e "\t-d <delay>\t-- delay between port scans in seconds"
+    echo -e "\t-r\t\t-- randomize port list"
+    echo -e "\t-f\t\t-- fast scan using /etc/services for common ports"
+    echo -e "\t-l <logfile>\t-- log results to a file"
+    echo -e "\t-h\t\t-- display this help message"
 }
 
-# Populate PORTINDEX array with service names
 populate_port_index() {
     if [ ! -r "/etc/services" ]; then
 	return
@@ -53,15 +57,23 @@ get_port_index() {
 
     echo $service
 }
+y
 
 # set defaults
 timeout=$DEFAULT_TIMEOUT
 ports=$DEFAULT_PORTS
+delay=$DEFAULT_DELAY
 
 # CLI args.
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
 while [ $# -gt 0 ]; do
     case $1 in
 	-h) usage;
+	    exit 0
 	    ;;
 	-t) target=$2
 	    shift
@@ -78,6 +90,15 @@ while [ $# -gt 0 ]; do
 	    shift
 	    ;;
 	-v) VERBOSE=$((VERBOSE+1))
+	    ;;
+	-d) delay=$2
+	    shift
+	    ;;
+	-r) RANDOMIZE=1
+	    ;;
+	-f) FAST_SCAN=1
+	    ;;
+	-l) LOG_FILE=$2
 	    shift
 	    ;;
 	-*) echo "[-] Unknown flag: $1"
@@ -110,6 +131,9 @@ fi
 # replace commas with space to make life easier on @dmfroberson
 ports=$(echo "$ports" | tr , ' ')
 
+# Initialize out variable
+out=""
+
 # deal with ranges of ports
 for token in $ports; do
     if [[ $token == *"-"* ]]; then
@@ -134,6 +158,16 @@ ports=$(echo "$out" | tr ' ' '\n' | sort -nu)
 echo "Done."
 
 populate_port_index
+
+# Fast scan using /etc/services for common ports
+if [ $FAST_SCAN -eq 1 ]; then
+    ports=$(grep -Eo '^[0-9]+' /etc/services | sort -nu | tr '\n' ' ')
+fi
+
+# Randomize port list if requested
+if [ $RANDOMIZE -eq 1 ]; then
+    ports=$(echo "$ports" | tr ' ' '\n' | shuf | tr '\n' ' ')
+fi
 
 # Do the scan.
 portcount=$(echo "$ports" | wc -w)
@@ -166,9 +200,16 @@ for port in $ports; do
     esac
 
     echo "[+] Port ${port}/${service} appears to be open."
+    if [ "$LOG_FILE" ]; then
+        echo "[+] Port ${port}/${service} appears to be open." >> "$LOG_FILE"
+    fi
 
     count=$((count+1))
+    sleep "$delay"
 done
 
 echo
 echo "[+] Done. $count ports open."
+if [ "$LOG_FILE" ]; then
+    echo "[+] Done. $count ports open." >> "$LOG_FILE"
+fi
